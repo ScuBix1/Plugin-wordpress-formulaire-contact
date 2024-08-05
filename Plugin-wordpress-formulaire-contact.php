d2e17d7612addb50848aea4136bb503d21134e71
ScuBix1 <bastian.monnin1@gmail.com>
2024-08-05 22:25:30 +0200

    creation of the custom tables

--- Plugin-wordpress-formulaire-contact.php
+++ Plugin-wordpress-formulaire-contact.php
@@ -10,4 +10,23 @@ License: GPL
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: Plugin-wordpress-contact-form
 */
+if(!function_exists('active_plugin_contact')){
+    function active_plugin_contact(){
+        global $wpdb;
+        $sql = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}contact_form(
+            id int not null auto_increment,
+            name varchar(200),
+            email varchar(100),
+            primary key(id)
+        )";
+        $wpdb->query($sql);
+    }
+}
+if(!function_exists('deactive_plugin_contact')){
+    function deactive_plugin_contact(){
+
+    }
+}
+register_activation_hook(__FILE__, 'active_plugin_contact');
+register_activation_hook(__FILE__, 'deactive_plugin_contact');
 ?>
