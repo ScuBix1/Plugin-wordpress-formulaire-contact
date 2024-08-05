6c312936e8f1bb6c76776aa087419ed77190a042
ScuBix1 <bastian.monnin1@gmail.com>
2024-08-05 20:42:57 +0200

    creation of the basic structure of plugin

---
+++ Plugin-wordpress-formulaire-contact.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Plugin Wordpress Contact Form
+Plugin URL: https://bastian-monnin.alwaysdata.net/
+Description: Add a contact form on your web site 
+Version: 1.0.1
+Author: Bastian Monnin
+Author URI: https://bastian-monnin.alwaysdata.net/
+License: GPL
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: Plugin-wordpress-contact-form
+*/
+?>
